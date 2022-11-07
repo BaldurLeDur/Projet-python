0380a8c031ef587143e5213d2a03fe7d3a6364e5
BaldurLeDur <robin.frouin.04@gmail.com>
2022-11-07 18:53:17 +0100

    exo

---
+++ exo.py
@@ -0,0 +1,27 @@
+#DEBUT
+def add(x,y):
+    return x + y
+def sub(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+def modulo(x,y):
+    return x % y
+add(2,3)
+sub(78,5)
+multiply(5,6)
+divide(9,3)
+modulo(96,4)
+print(add)
+print(sub)
+print(multiply)
+print(divide)
+print(modulo)
+
+#ex1
+"calculer salaire seconde, horaire, journé, an par jours ouvrés"
+#ex2
+"salaire net"
+#FIN
