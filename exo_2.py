b5d64dec1ea87c3713a67b382e53719a269a9b78
BaldurLeDur <robin.frouin.04@gmail.com>
2022-11-08 16:56:47 +0100

    exo en cours

--- exo_2.py
+++ exo_2.py
@@ -1,20 +1,6 @@
-def add(x,y):
-    return x + y
-def sub(x,y):
-    return x - y
-def multiply(x,y):
-    return x * y
-def divide(x,y):
-    return x / y
-def modulo(x,y):
-    return x % y
-
-# salire bordel
-salaire = 2450
-
-print("votre salire anuelle est de ",salaire," euro par an. Soit " ,divide(salaire,12), " euro par moi. Soit ", divide(divide(salaire,12),25), " euro par jour ouvrés. Soit ",divide(divide(divide(salaire,12),25),60), " euro par heure. Soit ", divide(divide(divide(divide(salaire,12),25),60),60), " euro par seconde")
-
-#salaire net
-
-print("votre slaire net est de ", sub(salaire,multiply(salaire,0.23)), " euro")
-
+salaire = int(input("quelle est votre salaire ? : "))
+fonction = int(input("pour fonction publique, tapez 1 et pour fonction privé tapez 2 : "))
+if fonction == 1:
+    print("votre salaire net est de " , salaire - salaire * 0.15, " euro")
+else:
+    print("votre salaire net est de " , salaire - salaire * 0.25, " euro")
