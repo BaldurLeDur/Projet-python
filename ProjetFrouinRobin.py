8d8c7c1c33487ce3d740d18b433fc53c1bc777b1
BaldurLeDur <robin.frouin.04@gmail.com>
2022-11-09 10:01:54 +0100

    exo cours

---
+++ ProjetFrouinRobin.py
@@ -0,0 +1,32 @@
+#Importer le module random
+#Importer le module time
+#créer une liste avec pierre feuille ciseaux
+#Créer variable winPlayerUn et la mettre à 0
+#Créer variable winPlayerDeux et la mettre à 0
+#Créer variable winPlayerRobot et la mettre à 0
+#Demander combien de joueur entre 1 et 2
+#Demander avec la variable round combien de round à gagner
+#Demender avec la variable typeJeu si il veut jouer au jeu classique (1) au jeu clasique mais à personalisé (2) au jeu à personalisé (3)
+#Si typeJeu est égal à 1
+    #Alors : tant que winPlayerUn < round ou winRobot < round
+        #Demander avec la variable choixJoueur que veut jouer le joueur
+        #Créer une variable choixRobot qui prend un string  au hasars dans la liste listeClassique
+        #Dormir 1 seconde
+        #Ecrire pierre
+        #Dormir 1 seconde
+        #Ecrire fueille
+        #Dormir 1 seconde
+        #Ecrire ciseuax
+        #dormir 1 seconde
+        #Ecrire "Choix robot : ",choixRobot," VS Choix joueur : ",choixJoueur
+        #dormir 1 seconde
+        #Si choixRobot == choixJoueur
+            #Alors : ecrire "égalité"
+        #Sinon si choixRobot == "pierre" et choixJoueur == "feuille"
+            #Alors : ecrire "le robot à gagner et tu a perdue"
+            #Incrementer 1 à winRobot
+            #Dormir 0.5 seconde
+            #Ecrire "le robot est à ",winRobot, "victoire et le joueur est à ",winJoueur
+    #Si winPLayerUn == 5
+        #Alors : écrire "tu as gagner"
+    #Sinon : écrire "tu as perdu"
--- exo_2.py
+++ exo_2.py
@@ -19,9 +19,9 @@
     #    print(x/y)
 
 #importer le module temps
-import time
-import random
-caracteres = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789"
+#import time
+#import random
+#caracteres = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789"
 
 #def input():
     #Renvoie un caractère de type string au hasard
@@ -44,34 +44,69 @@ caracteres = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789"
 #Sinon
     #Alors ecrire "dis le si je te fais chier"
 
-jouer = str(input("Veuxt tu joueur au DEVINE OU TU PERDS ? Le but est simple un robot choisie un carcatèrealéatoire, tu dois le deviner. Réponds oui si tu veux jouer et non si tu ne veux pas. : "))
-if jouer == "oui":
-    while jouer == "oui":
-        charJoueur = str(input("Devine quelle caractère le robot vas choisir : "))
-        h = caracteres[random.randint(0, len(caracteres)-1)]
-        if charJoueur != h:
-            print("Le caractère que le robot a choisi est : ",h, ", alors que le tiens est : ",charJoueur,". Tu as perdu")
-            time.sleep(1.5)
-            print("T'es une merde")
-            time.sleep(1)
-            jouer = str(input("Tu veux rejouer. Repond oui ou non ? : "))
-        else:
-            print("bien ouèj batard t'as trouvé")
-            time.sleep(1)
-            jouer = str(input("Tu veux rejouer. Repond oui ou non ? : "))
-elif jouer == "non":
-    print("bah dis le si je te fais chier")
-else:
-    print("azy écris bien aussi batard !")
+#jouer = str(input("Veuxt tu joueur au DEVINE OU TU PERDS ? Le but est simple un robot choisie un carcatèrealéatoire, tu dois le deviner. Réponds oui si tu veux jouer et non si tu ne veux pas. : "))
+#if jouer == "oui":
+ #   while jouer == "oui":
+  #      charJoueur = str(input("Devine quelle caractère le robot vas choisir : "))
+   #     h = caracteres[random.randint(0, len(caracteres)-1)]
+    #    if charJoueur != h:
+     #       print("Le caractère que le robot a choisi est : ",h, ", alors que le tiens est : ",charJoueur,". Tu as perdu")
+      #      time.sleep(1.5)
+       #     print("T'es une merde")
+        #    time.sleep(1)
+         #   jouer = str(input("Tu veux rejouer. Repond oui ou non ? : "))
+        #else:
+         #   print("bien ouèj batard t'as trouvé")
+          #  time.sleep(1)
+           # jouer = str(input("Tu veux rejouer. Repond oui ou non ? : "))
+#elif jouer == "non":
+#    print("bah dis le si je te fais chier")
+#else:
+ #   print("azy écris bien aussi batard !")
 
 #exercice 1
 #faire une fonnction qui concatene 2 chaine de caractère, les séparent par une virgule
-
+#def char (charUn,CharDeux):
+#    return charUn + ", " + CharDeux
+#print(char("hello","world"))
 #exercie 2
 #fire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de caractere
 #avec l'ensembles des occurence d'un chiffre e.g.:
-#pour tableau = [0,1,1,1,0,1,1,0,1]
+tableau = [0,1,1,1,0,1,1,0,1]
 #la fonction(tableau, 0) doit renvoyez "0, 4, 7" n'hésitez pas a implanter la première fonction
+#Définir un boolen tels que firstTurn est true
+firstTurn = True
+def concatWithComma(strA,strB):
+    strAChange = str(strA)
+    strBChange = str(strB)
+    return strAChange + ', ' + strBChange
+#definir la fonctio findIndex qui itere sur tableau, cherchant l'index des différentes occurences de x
+def findIndex(tableau,x):
+    #Definir l'index de départ i
+    i = 0
+    #Definir chaineRetour telle qu'une chaine de caractère vide
+    chaineRetour = ''
+    #Tant que i est différent du nombre d'élément dans le tableu
+    while i != len(tableau):
+        #Alors j'attribue une variable la valeur de tableau à l'index i
+        selected = tableau[i]
+        #Si selected est égal à x ET que firstTurn
+        if selected == x and selected == firstTurn:
+            #Alors on assigne à chaineRetour le retour de str(i)
+            chaineRetour = str(i)
+            #Changer la valeur de firstTurn a false
+            firstTurn = False
+        #Si selected = x
+        if selected == x:
+            #Alors j'assigne le retour de concatWithComma tel que : concatWithComma(chaineRetour, i) à chaineRetour
+            chaineRetour = concatWithComma(chaineRetour,i)
+        i = i + 1
+    return chaineRetour
+
+
+
+
+
 
 #exercice 3
 #renvoyer / afficher un message
